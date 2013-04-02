c998870fe9fb344457d0e10c3aa5745e21e97845
Sarva Mangala <sarvamangala.rudramuniyappa@sage.com>
2013-04-02 16:31:08 -0400

    Initial Commit

---
+++ Hellogit/Hellogit/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Hellogit")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Sage")> 
+<Assembly: AssemblyProduct("Hellogit")> 
+<Assembly: AssemblyCopyright("Copyright © Sage 2013")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("513ca7da-e91a-4437-9352-482c7f5beee2")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
